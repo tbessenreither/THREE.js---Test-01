b2ca6c4c6cdef03b96a27da6ba35778b74aab826
tbessenreither <github@adt-it.de>
2022-04-04 19:56:26 +0200

    feat(Subscribers): added send old data capability default false;

--- src/classes/Subscribers.ts
+++ src/classes/Subscribers.ts
@@ -3,10 +3,21 @@ class Subscribers {
 	subscribers = [];
 	subscriberNextId = 0;
 
+	sendOldData = false;
+	lastData = null;
+
+	constructor(sendOldData = false) {
+		this.sendOldData = sendOldData;
+	}
 
 	register(callback) {
 		let id = this.subscriberNextId++;
 		this.subscribers[id] = callback;
+
+		if (this.sendOldData && this.lastData) {
+			callback(...this.lastData);
+		}
+
 		return id;
 	}
 
@@ -15,6 +26,9 @@ class Subscribers {
 	}
 	
 	call(...args) {
+		if (this.sendOldData) {
+			this.lastData = args;
+		}
 		for (let key in this.subscribers) {
 			this.subscribers[key](...args);
 		}
